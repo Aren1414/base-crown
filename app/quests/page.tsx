81468f1776350c1f0dc1f2d440a7d6a4a6aa31bc
Aren <Aren.silver12@gmail.com>
2026-06-05 16:32:29 +0330

    Create QuestsPage component with basic layout

---
+++ app/quests/page.tsx
@@ -0,0 +1,7 @@
+export default function QuestsPage() {
+  return (
+    <main className="min-h-screen bg-black text-white p-4">
+      <h1 className="text-3xl font-bold">Quests</h1>
+    </main>
+  );
+}
