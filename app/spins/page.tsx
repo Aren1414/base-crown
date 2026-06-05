33d906f39783a20160d4c9cf74867966b587e22a
Aren <Aren.silver12@gmail.com>
2026-06-05 16:30:45 +0330

    Create SpinsPage component with basic layout

---
+++ app/spins/page.tsx
@@ -0,0 +1,7 @@
+export default function SpinsPage() {
+  return (
+    <main className="min-h-screen bg-black text-white p-4">
+      <h1 className="text-3xl font-bold">Spins</h1>
+    </main>
+  );
+}
