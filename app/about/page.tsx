c3a534b1220d5c76d5a62c96f79a975dd4d1c564
Aren <Aren.silver12@gmail.com>
2026-06-05 16:35:08 +0330

    Create AboutPage component with basic layout

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <main className="min-h-screen bg-black text-white p-4">
+      <h1 className="text-3xl font-bold">About</h1>
+    </main>
+  );
+}
