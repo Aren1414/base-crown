8360460f4264aaea473247549844bdcf8db34ee9
Aren <Aren.silver12@gmail.com>
2026-06-05 16:31:43 +0330

    Create RelicsPage component with basic layout

---
+++ app/relics/page.tsx
@@ -0,0 +1,7 @@
+export default function RelicsPage() {
+  return (
+    <main className="min-h-screen bg-black text-white p-4">
+      <h1 className="text-3xl font-bold">Relics</h1>
+    </main>
+  );
+}
