3ab057d8deee1052967859ec9ad892f106e86867
Aren <Aren.silver12@gmail.com>
2026-06-05 16:34:00 +0330

    Create LeaderboardPage component

---
+++ app/leaderboard/page.tsx
@@ -0,0 +1,7 @@
+export default function LeaderboardPage() {
+  return (
+    <main className="min-h-screen bg-black text-white p-4">
+      <h1 className="text-3xl font-bold">Leaderboard</h1>
+    </main>
+  );
+}
