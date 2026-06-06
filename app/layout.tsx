92a8e07638f30d4032416759cec3d7715ceaadb1
Aren <Aren.silver12@gmail.com>
2026-06-06 16:43:48 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 import BottomNav from "@/components/BottomNav";
-import Providers from "@/app/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,13 +29,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-black text-white">
-        <Providers>
-          <div className="flex-1 pb-16">
-            {children}
-          </div>
+        <div className="flex-1 pb-16">
+          {children}
+        </div>
 
-          <BottomNav />
-        </Providers>
+        <BottomNav />
       </body>
     </html>
   );
