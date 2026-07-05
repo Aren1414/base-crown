f86fab4137c26cfafc3ef565585c6882bbd59889
Aren <Aren.silver12@gmail.com>
2026-07-05 15:55:02 +0330

    Rename GamePage to PrankSquadGame and update scene

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -3,39 +3,61 @@
 import { useEffect, useRef } from 'react';
 import Phaser from 'phaser';
 
-export default function GamePage() {
+export default function PrankSquadGame() {
   const gameRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (!gameRef.current) return;
 
-    const config = {
+    class PrankScene extends Phaser.Scene {
+      constructor() {
+        super({ key: 'PrankScene' });
+      }
+
+      preload() {
+        this.load.image('background', 'https://labs.phaser.io/assets/skies/space3.png');
+        this.load.image('runner', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
+      }
+
+      create() {
+        this.add.image(400, 300, 'background');
+        
+        const runner = this.physics.add.sprite(100, 450, 'runner');
+        runner.setBounce(0.2);
+        runner.setCollideWorldBounds(true);
+
+        this.add.text(100, 50, 'Prank Squad - Tap to Jump & Prank!', {
+          fontSize: '20px',
+          color: '#fff'
+        });
+      }
+    }
+
+    const config: Phaser.Types.Core.GameConfig = {
       type: Phaser.AUTO,
       width: 800,
       height: 600,
       parent: gameRef.current,
       physics: {
         default: 'arcade',
-        arcade: { gravity: { y: 300 }, debug: false }
+        arcade: {
+          gravity: { y: 300 },
+          debug: false
+        }
       },
-      scene: {
-        preload: function() {
-          this.load.image('sky', 'https://labs.phaser.io/assets/skies/space3.png');
-          this.load.image('ground', 'https://labs.phaser.io/assets/sprites/platform.png');
-        },
-        create: function() {
-          this.add.image(400, 300, 'sky');
-          const ground = this.physics.add.staticGroup();
-          ground.create(400, 568, 'ground').setScale(2).refreshBody();
-        },
-        update: function() {}
-      }
+      scene: PrankScene
     };
 
     const game = new Phaser.Game(config);
 
-    return () => game.destroy(true);
+    return () => {
+      game.destroy(true);
+    };
   }, []);
 
-  return <div ref={gameRef} className="w-full h-screen bg-black" />;
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <div ref={gameRef} className="border border-gray-700 rounded-lg overflow-hidden" />
+    </div>
+  );
 }
