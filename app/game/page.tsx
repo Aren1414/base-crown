a8105530efdb308e15064df4d93dec3f7c481959
Aren <Aren.silver12@gmail.com>
2026-07-05 15:57:12 +0330

    Refactor Phaser scene to use object literal syntax

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -9,30 +9,6 @@ export default function PrankSquadGame() {
   useEffect(() => {
     if (!gameRef.current) return;
 
-    class PrankScene extends Phaser.Scene {
-      constructor() {
-        super({ key: 'PrankScene' });
-      }
-
-      preload() {
-        this.load.image('background', 'https://labs.phaser.io/assets/skies/space3.png');
-        this.load.image('runner', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
-      }
-
-      create() {
-        this.add.image(400, 300, 'background');
-        
-        const runner = this.physics.add.sprite(100, 450, 'runner');
-        runner.setBounce(0.2);
-        runner.setCollideWorldBounds(true);
-
-        this.add.text(100, 50, 'Prank Squad - Tap to Jump & Prank!', {
-          fontSize: '20px',
-          color: '#fff'
-        });
-      }
-    }
-
     const config: Phaser.Types.Core.GameConfig = {
       type: Phaser.AUTO,
       width: 800,
@@ -45,7 +21,31 @@ export default function PrankSquadGame() {
           debug: false
         }
       },
-      scene: PrankScene
+      scene: {
+        preload: function (this: Phaser.Scene) {
+          this.load.image('sky', 'https://labs.phaser.io/assets/skies/space3.png');
+          this.load.image('ground', 'https://labs.phaser.io/assets/sprites/platform.png');
+          this.load.image('runner', 'https://labs.phaser.io/assets/sprites/phaser-dude.png');
+        },
+        create: function (this: Phaser.Scene) {
+          this.add.image(400, 300, 'sky');
+          
+          const platforms = this.physics.add.staticGroup();
+          platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+          const runner = this.physics.add.sprite(100, 450, 'runner');
+          runner.setBounce(0.2);
+          runner.setCollideWorldBounds(true);
+
+          this.physics.add.collider(runner, platforms);
+
+          this.add.text(100, 50, 'Prank Squad - Tap to Jump!', {
+            fontSize: '24px',
+            color: '#ffffff'
+          });
+        },
+        update: function () {}
+      }
     };
 
     const game = new Phaser.Game(config);
@@ -56,8 +56,10 @@ export default function PrankSquadGame() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center">
-      <div ref={gameRef} className="border border-gray-700 rounded-lg overflow-hidden" />
+    <div className="min-h-screen bg-zinc-950 flex items-center justify-center p-4">
+      <div className="border border-zinc-800 rounded-xl overflow-hidden shadow-2xl">
+        <div ref={gameRef} className="w-[800px] h-[600px]" />
+      </div>
     </div>
   );
 }
