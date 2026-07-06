d0225f7006811d7ef68361aff9dd8093eb85e33f
Aren <Aren.silver12@gmail.com>
2026-07-06 17:51:49 +0330

    Replace PhaserGame with ChaosLane3D component

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,17 +1,13 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import { useEffect, useRef } from 'react';
 
-const PhaserGame = dynamic(
-  () => import('./PhaserGame'),
-  { ssr: false }
-);
+const ChaosLane3D = dynamic(() => import('./ChaosLane3D'), { ssr: false });
 
 export default function GamePage() {
   return (
-    <div className="min-h-screen bg-zinc-950">
-      <PhaserGame />
+    <div className="min-h-screen bg-black overflow-hidden">
+      <ChaosLane3D />
     </div>
   );
 }
