6a882b70f239eb8c353f6d44464d7055d1309660
Aren <Aren.silver12@gmail.com>
2026-07-07 17:50:35 +0330

    Add animations module with various animation links

---
+++ src/animations/index.js
@@ -0,0 +1,14 @@
+export const animations = {
+  hit_head: "https://pixeldrain.com/l/ou6WZuKR#item=0",
+  walk: "https://pixeldrain.com/l/ou6WZuKR#item=1",
+  idle: "https://pixeldrain.com/l/ou6WZuKR#item=2",
+  death: "https://pixeldrain.com/l/ou6WZuKR#item=3",
+  hit_side: "https://pixeldrain.com/l/ou6WZuKR#item=4",
+  kick_crescent: "https://pixeldrain.com/l/ou6WZuKR#item=5",
+  landing: "https://pixeldrain.com/l/ou6WZuKR#item=6",
+  kick_basic: "https://pixeldrain.com/l/ou6WZuKR#item=7",
+  kick_mma: "https://pixeldrain.com/l/ou6WZuKR#item=8",
+  punch_bag: "https://pixeldrain.com/l/ou6WZuKR#item=9",
+  run: "https://pixeldrain.com/l/ou6WZuKR#item=10",
+  punch_uppercut: "https://pixeldrain.com/l/ou6WZuKR#item=11",
+};
