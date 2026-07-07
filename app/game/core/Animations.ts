a84b32f67254c8c992dd7ce69d030f9dec4efbdb
Aren <Aren.silver12@gmail.com>
2026-07-07 20:49:06 +0330

    Add animation URLs to Animations.ts

---
+++ app/game/core/Animations.ts
@@ -0,0 +1,14 @@
+export const animations = {
+  hit_head: "https://github.com/Aren1414/base-crown/releases/download/game/hit_head.glb",
+  walk: "https://github.com/Aren1414/base-crown/releases/download/game/walk.glb",
+  idle: "https://github.com/Aren1414/base-crown/releases/download/game/idle.glb",
+  death: "https://github.com/Aren1414/base-crown/releases/download/game/Death.glb",
+  hit_side: "https://github.com/Aren1414/base-crown/releases/download/game/hit_side.glb",
+  kick_crescent: "https://github.com/Aren1414/base-crown/releases/download/game/kick_crescent.glb",
+  landing: "https://github.com/Aren1414/base-crown/releases/download/game/landing.glb",
+  kick_basic: "https://github.com/Aren1414/base-crown/releases/download/game/kick_basic.glb",
+  kick_mma: "https://github.com/Aren1414/base-crown/releases/download/game/kick_mma.glb",
+  punch_bag: "https://github.com/Aren1414/base-crown/releases/download/game/punch_bag.glb",
+  run: "https://github.com/Aren1414/base-crown/releases/download/game/run.glb",
+  punch_uppercut: "https://github.com/Aren1414/base-crown/releases/download/game/punch_uppercut.glb",
+};
