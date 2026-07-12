7048d5014eaaeb069604316c8b2ef15c7fa7ee64
Aren <Aren.silver12@gmail.com>
2026-07-12 14:45:29 +0330

    Fix string quotes and formatting in ChaosLane3D.tsx

--- app/game/ChaosLane3D.tsx
+++ app/game/ChaosLane3D.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
@@ -219,4 +219,4 @@ export default function ChaosLane3D() {
       </div>
     </div>
   );
-          }
+            }
