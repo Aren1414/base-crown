c0bc1c8a54912394495036d21e942c9a36ef7a95
Aren <Aren.silver12@gmail.com>
2026-07-25 02:22:30 +0330

    Remove file extension from dynamic import

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -2,7 +2,7 @@
 
 import dynamic from 'next/dynamic';
 
-const ChaosLane3D = dynamic(() => import('./ChaosLane3D.tsx'), {
+const ChaosLane3D = dynamic(() => import('./ChaosLane3D'), {
   ssr: false,
   loading: () => null,
 });
