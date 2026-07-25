5d5a9c5182e05a4a5d182e1f01e9c6d677acb27a
Aren <Aren.silver12@gmail.com>
2026-07-25 15:43:11 +0330

    Add urban and forest model assets

---
+++ app/game/assets/Models.ts
@@ -0,0 +1,88 @@
+export const URBAN_STREETS = [
+  { url: "streets/Street1.glb", scale: 7.5 },
+  { url: "streets/Street2.glb", scale: 7.5 },
+  { url: "streets/Street3.glb", scale: 7.5 },
+];
+
+export const URBAN_ALLEYS = [
+  { url: "alleys/Alley1.glb", scale: 7.5 },
+  { url: "alleys/Alley2.glb", scale: 7.5 },
+  { url: "alleys/Alley3.glb", scale: 7.5 },
+  { url: "Connecting_alley_and_street/Connecting_alley_and_street.glb", scale: 7.5 },
+];
+
+export const URBAN_BUILDINGS = [
+  { url: "Buildings/Urban_building1.glb", scale: 4.8 },
+  { url: "Buildings/Urban_building2.glb", scale: 5.2 },
+  { url: "Buildings/Urban_building3.glb", scale: 5.5 },
+  { url: "Buildings/Urban_building4.glb", scale: 5.8 },
+  { url: "Buildings/Urban_building5.glb", scale: 6.0 },
+  { url: "Buildings/Urban_building6.glb", scale: 6.2 },
+  { url: "Buildings/Urban_building7.glb", scale: 6.4 },
+  { url: "Buildings/Urban_building8.glb", scale: 6.6 },
+  { url: "Buildings/Urban_building9.glb", scale: 6.8 },
+  { url: "Buildings/Urban_building10.glb", scale: 7.0 },
+  { url: "Buildings/Urban_building11.glb", scale: 7.2 },
+  { url: "Buildings/Villa_house1.glb", scale: 3.6 },
+  { url: "Buildings/Villa_house2.glb", scale: 3.8 },
+  { url: "Buildings/Villa_house3.glb", scale: 4.0 },
+];
+
+export const URBAN_VEHICLES = [
+  { url: "vehicles/Ambulance_car.glb", scale: 3.2 },
+  { url: "vehicles/Motorcycle.glb", scale: 2.4 },
+  { url: "vehicles/Pickup_truck.glb", scale: 3.4 },
+  { url: "vehicles/Police_car.glb", scale: 3.0 },
+  { url: "vehicles/Sports_car1.glb", scale: 3.0 },
+  { url: "vehicles/Sports_car2.glb", scale: 3.0 },
+  { url: "vehicles/Sports_car3.glb", scale: 3.0 },
+  { url: "vehicles/Van_car.glb", scale: 3.4 },
+];
+
+export const FOREST_TREES = [
+  "Plants_and_trees/glTF/BirchTree_1.gltf",
+  "Plants_and_trees/glTF/BirchTree_2.gltf",
+  "Plants_and_trees/glTF/BirchTree_3.gltf",
+  "Plants_and_trees/glTF/BirchTree_4.gltf",
+  "Plants_and_trees/glTF/BirchTree_5.gltf",
+  "Plants_and_trees/glTF/MapleTree_1.gltf",
+  "Plants_and_trees/glTF/MapleTree_2.gltf",
+  "Plants_and_trees/glTF/MapleTree_3.gltf",
+  "Plants_and_trees/glTF/MapleTree_4.gltf",
+  "Plants_and_trees/glTF/MapleTree_5.gltf",
+  "Plants_and_trees/glTF/DeadTree_1.gltf",
+  "Plants_and_trees/glTF/DeadTree_2.gltf",
+  "Plants_and_trees/glTF/DeadTree_3.gltf",
+  "Plants_and_trees/glTF/DeadTree_4.gltf",
+  "Plants_and_trees/glTF/DeadTree_5.gltf",
+  "Plants_and_trees/glTF/DeadTree_6.gltf",
+  "Plants_and_trees/glTF/DeadTree_7.gltf",
+  "Plants_and_trees/glTF/DeadTree_8.gltf",
+  "Plants_and_trees/glTF/DeadTree_9.gltf",
+  "Plants_and_trees/glTF/DeadTree_10.gltf",
+];
+
+export const FOREST_BUSHES = [
+  "Plants_and_trees/glTF/Bush.gltf",
+  "Plants_and_trees/glTF/Bush_Small.gltf",
+  "Plants_and_trees/glTF/Bush_Large.gltf",
+  "Plants_and_trees/glTF/Bush_Flowers.gltf",
+  "Plants_and_trees/glTF/Bush_Large_Flowers.gltf",
+  "Plants_and_trees/glTF/Bush_Small_Flowers.gltf",
+];
+
+export const FOREST_GRASS = [
+  "Plants_and_trees/glTF/Grass_Large.gltf",
+  "Plants_and_trees/glTF/Grass_Large_Extruded.gltf",
+  "Plants_and_trees/glTF/Grass_Small.gltf",
+];
+
+export const FOREST_FLOWERS = [
+  "Plants_and_trees/glTF/Flower_1.gltf",
+  "Plants_and_trees/glTF/Flower_1_Clump.gltf",
+  "Plants_and_trees/glTF/Flower_2.gltf",
+  "Plants_and_trees/glTF/Flower_2_Clump.gltf",
+  "Plants_and_trees/glTF/Flower_3_Clump.gltf",
+  "Plants_and_trees/glTF/Flower_4_Clump.gltf",
+  "Plants_and_trees/glTF/Flower_5_Clump.gltf",
+];
