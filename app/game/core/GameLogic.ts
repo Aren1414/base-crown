fd068e33fe54085edae817b6e86c28545132fa73
Aren <Aren.silver12@gmail.com>
2026-07-28 14:49:23 +0330

    Update GameLogic.ts

--- app/game/core/GameLogic.ts
+++ app/game/core/GameLogic.ts
@@ -3,8 +3,8 @@ import * as THREE from "three";
 export function createGameLogic(
   player: THREE.Object3D
 ) {
-  const walkSpeed = 4;
-  const runSpeed = 15.5;
+  const walkSpeed = 5;
+  const runSpeed = 11.5;
   const runThresholdSquared = 0.36;
   const minimumInputSquared = 0.0001;
   const moveDirection = new THREE.Vector3();
